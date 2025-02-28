fb88e5148127bd141d4dda727d8027ad5284eed2
Halil <ibayy93@gmail.com>
2025-02-27 21:26:30 -0800

    added modal

---
+++ scripts/index.js
@@ -0,0 +1,28 @@
+const initialCards = [
+  {
+    name: "snowboarding",
+    link: "https://unsplash.com/photos/a-group-of-chairs-sitting-in-front-of-a-large-black-ball-4FcECUfOsO8",
+  },
+  {
+    name: "group of chairs",
+    link: "https://unsplash.com/photos/a-man-flying-through-the-air-while-riding-a-snowboard-vIrsbgXSjj0",
+  },
+  {
+    name: "A table with flowers",
+    link: "https://unsplash.com/photos/a-table-topped-with-a-vase-filled-with-flowers-vtKr_wHuNHg",
+  },
+  {
+    name: "Pair of earrings",
+    link: "https://unsplash.com/photos/a-close-up-of-a-person-wearing-a-pair-of-earrings-9uRta3OGvDo",
+  },
+  {
+    name: "City view",
+    link: "https://unsplash.com/photos/a-view-of-a-city-from-the-water-xLQM7Tnqdpg",
+  },
+  {
+    name: "Blurry photo of night",
+    link: "https://unsplash.com/photos/a-blurry-photo-of-a-city-at-night-4Kre8hBbBKo",
+  },
+];
+
+console.log(initialCards);
