f740b1a0c934da344c094b1eca34c4c1d6264dd9
Halil <ibayy93@gmail.com>
2025-03-24 15:00:49 -0700

    added imgs

--- scripts/index.js
+++ scripts/index.js
@@ -1,27 +1,27 @@
 const initialCards = [
   {
     name: "snowboarding",
-    link: "https://unsplash.com/photos/a-group-of-chairs-sitting-in-front-of-a-large-black-ball-4FcECUfOsO8",
+    link: "https://plus.unsplash.com/premium_photo-1739955024115-e2d219766d49?q=80&w=1932&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   },
   {
     name: "group of chairs",
-    link: "https://unsplash.com/photos/a-man-flying-through-the-air-while-riding-a-snowboard-vIrsbgXSjj0",
+    link: "https://images.unsplash.com/photo-1739911013843-0380d6504480?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   },
   {
     name: "A table with flowers",
-    link: "https://unsplash.com/photos/a-table-topped-with-a-vase-filled-with-flowers-vtKr_wHuNHg",
+    link: "https://images.unsplash.com/photo-1739909198159-a834175bd911?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   },
   {
     name: "Pair of earrings",
-    link: "https://unsplash.com/photos/a-close-up-of-a-person-wearing-a-pair-of-earrings-9uRta3OGvDo",
+    link: "https://plus.unsplash.com/premium_photo-1739548332624-399962d3a265?q=80&w=2014&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   },
   {
     name: "City view",
-    link: "https://unsplash.com/photos/a-view-of-a-city-from-the-water-xLQM7Tnqdpg",
+    link: "https://images.unsplash.com/photo-1739932215472-15ebf0ab6cf4?q=80&w=1965&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   },
   {
     name: "Blurry photo of night",
-    link: "https://unsplash.com/photos/a-blurry-photo-of-a-city-at-night-4Kre8hBbBKo",
+    link: "https://images.unsplash.com/photo-1734615106647-937d3996caf2?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   },
 ];
 
@@ -36,6 +36,23 @@ const editModalNameInput = editModal.querySelector("#profile-name-input");
 const editModalDescriptionInput = editModal.querySelector(
   "#profile-description-input"
 );
+
+const cardTemplate = document.querySelector("#card-template");
+const cardList = document.querySelector(".cards__list");
+
+function getCardElement(data) {
+  const cardElement = cardTemplate.content
+    .querySelector(".card")
+    .cloneNode(true);
+
+  const cardNameEl = cardElement.querySelector(".card__title");
+  const cardImgEl = cardElement.querySelector(".card__image");
+
+  cardNameEl.textContent = data.name;
+  cardImgEl.setAttribute("src", data.link);
+
+  return cardElement;
+}
 
 function openModal() {
   editModalNameInput.value = profileName.textContent;
@@ -59,3 +76,8 @@ profileEditButton.addEventListener("click", openModal);
 editModalCloseBtn.addEventListener("click", closeModal);
 
 editFormElement.addEventListener("submit", handleEditFormSubmit);
+
+for (let i = 0; i < initialCards.length; i++) {
+  const cardElement = getCardElement(initialCards[i]);
+  cardList.prepend(cardElement);
+}
